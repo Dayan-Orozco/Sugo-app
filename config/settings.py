83185d942edb7cca695c22e1932486463d15f323
Dayan-Dev <dayancitodor39@gmail.com>
2025-11-28 19:36:35 -0500

    Estado para la web correccion 1

--- config/settings.py
+++ config/settings.py
@@ -149,7 +149,7 @@ STATICFILES_DIRS = [
 ]
 
 # Aquí indicamos que collectstatic también copie en 'static'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
